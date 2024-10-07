88785e429c33d8e60cb195cf06a4f271021260c9
Francisco Javier Suazo <fjs9244@nyu.edu>
2024-10-07 13:24:13 -0400

    Update Homework1 Exercise4.py

--- Homework1 Exercise4.py
+++ Homework1 Exercise4.py
@@ -5,4 +5,4 @@ integer1 = int(input("Enter the first integer: "))
 integer2 = int(input("Enter the second integer: "))
 
 result = sum_of_integers(integer1, integer2)
-print("The sum is:", result)
+print("The result of the sum is:", result)
